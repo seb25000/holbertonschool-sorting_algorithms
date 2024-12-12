10526c933951a21c126242d6b2deac5f220c6f5b
seb25000 <seb25000@gmail.com>
2024-12-12 18:58:23 +0100

    1-insertion_sort_list.c

--- sort.h
+++ sort.h
@@ -14,6 +14,7 @@ struct listint_s *next;
 } listint_t;
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+listint_t *swap_node(listint_t *node, listint_t **list);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
