45a5199f4817501730b4b00cc0e03c9518ca9f5c
seb25000 <seb25000@gmail.com>
2024-12-15 15:19:19 +0100

    0-bubble_sort.c

--- sort.h
+++ sort.h
@@ -1,7 +1,21 @@
 #ifndef SORT_H
 #define SORT_H
 #include <stdlib.h>
+#include <stdlib.h>
+/*Comparison direction macros for bitonic sort*/
+#define UP 0
+#define DOWN 1
+
+/*enum bool - Enumeration of boolean values*/
+typedef enum bool
+{
+    false = 0,
+    true
+} bool;
+
 /**
+ * struct listint_s - Doubly linked list node
+ *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
