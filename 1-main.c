1f352f937ca95b88d5abf6b1011d544ecdb8fe14
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-12-16 11:23:04 +0100

    1-insertion_sort_list.c

--- 1-main.c
+++ 1-main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
-<<<<<<< HEAD
 
-=======
->>>>>>> 28df2684cf5344ecb04abb54b05a11afceced655
 /**
  * create_listint - Creates a doubly linked list from an array of integers
  *
@@ -15,7 +12,6 @@
  */
 listint_t *create_listint(const int *array, size_t size)
 {
-<<<<<<< HEAD
     listint_t *list;
     listint_t *node;
     int *tmp;
@@ -37,27 +33,6 @@ listint_t *create_listint(const int *array, size_t size)
     return (list);
 }
 
-=======
-listint_t *list;
-listint_t *node;
-int *tmp;
-list = NULL;
-while (size--)
-{
-node = malloc(sizeof(*node));
-if (!node)
-return (NULL);
-tmp = (int *)&node->n;
-*tmp = array[size];
-node->next = list;
-node->prev = NULL;
-list = node;
-if (list->next)
-list->next->prev = list;
-}
-return (list);
-}
->>>>>>> 28df2684cf5344ecb04abb54b05a11afceced655
 /**
  * main - Entry point
  *
@@ -65,7 +40,6 @@ return (list);
  */
 int main(void)
 {
-<<<<<<< HEAD
     listint_t *list;
     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
@@ -79,18 +53,4 @@ int main(void)
     printf("\n");
     print_list(list);
     return (0);
-=======
-listint_t *list;
-int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-size_t n = sizeof(array) / sizeof(array[0]);
-list = create_listint(array, n);
-if (!list)
-return (1);
-print_list(list);
-printf("\n");
-insertion_sort_list(&list);
-printf("\n");
-print_list(list);
-return (0);
->>>>>>> 28df2684cf5344ecb04abb54b05a11afceced655
 }
