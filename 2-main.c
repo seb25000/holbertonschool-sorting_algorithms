ad38bcfd21050e9dcdc122a8458f4bb4b5aa4348
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-12-16 11:27:46 +0100

    2-selection_sort.c

--- 2-main.c
+++ 2-main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
-<<<<<<< HEAD
 
-=======
->>>>>>> 28df2684cf5344ecb04abb54b05a11afceced655
 /**
  * main - Entry point
  *
@@ -12,7 +9,6 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
 
@@ -22,14 +18,4 @@ int main(void)
     printf("\n");
     print_array(array, n);
     return (0);
-=======
-int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-size_t n = sizeof(array) / sizeof(array[0]);
-print_array(array, n);
-printf("\n");
-selection_sort(array, n);
-printf("\n");
-print_array(array, n);
-return (0);
->>>>>>> 28df2684cf5344ecb04abb54b05a11afceced655
 }
